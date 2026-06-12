f330d79d9d628160accfa6bca85820737051abc2
Sebastian <sbbastian0@gmail.com>
2026-06-12 16:19:35 -0500

    Actualiza resultado de Canada vs Bosnia

--- src/data/results/matchResults.ts
+++ src/data/results/matchResults.ts
@@ -5,6 +5,7 @@ export const MATCH_RESULTS: MatchResults = {
   match1: { t1: 2, t2: 0 },
   // Confirmado el 11 de junio de 2026: Corea del Sur 2 - 1 República Checa.
   match2: { t1: 2, t2: 1 },
-  match3: { t1: '', t2: '' },
+  // Confirmado el 12 de junio de 2026: Canadá 1 - 1 Bosnia.
+  match3: { t1: 1, t2: 1 },
   match4: { t1: '', t2: '' },
 }
