c98aca5c937d71fe6f77e9994a9f7244c6a6e718
Sebastian <sbbastian0@gmail.com>
2026-06-13 07:34:52 -0500

    Actualiza resultado de Estados Unidos vs Paraguay

--- src/data/results/matchResults.ts
+++ src/data/results/matchResults.ts
@@ -7,5 +7,6 @@ export const MATCH_RESULTS: MatchResults = {
   match2: { t1: 2, t2: 1 },
   // Confirmado el 12 de junio de 2026: Canadá 1 - 1 Bosnia.
   match3: { t1: 1, t2: 1 },
-  match4: { t1: '', t2: '' },
+  // Confirmado el 12 de junio de 2026: Estados Unidos 4 - 1 Paraguay.
+  match4: { t1: 4, t2: 1 },
 }
