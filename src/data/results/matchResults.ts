01f18b02532d96cd1591e173252c766827fa4f6b
Sebastian <sbbastian0@gmail.com>
2026-06-18 18:45:01 -0500

    Corrige resultados parciales del 18 de junio

--- src/data/results/matchResults.ts
+++ src/data/results/matchResults.ts
@@ -53,7 +53,6 @@ export const MATCH_RESULTS: MatchResults = {
   match25: { t1: 1, t2: 1 },
   // Confirmado el 18 de junio de 2026: Suiza 4 - 1 Bosnia.
   match26: { t1: 4, t2: 1 },
-  // Confirmado el 18 de junio de 2026: Canadá 4 - 0 Qatar.
-  match27: { t1: 4, t2: 0 },
+  match27: { t1: '', t2: '' },
   match28: { t1: '', t2: '' },
 }
